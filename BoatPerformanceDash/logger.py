f59058f337b463702f296e9bc15f130139bec80e
jesper <jesper.hellstrm@gmail.com>
2026-07-30 22:37:07 +0200

    Fixed logging format

--- BoatPerformanceDash/logger.py
+++ BoatPerformanceDash/logger.py
@@ -21,7 +21,10 @@ def udp_listener():
     print(f"Lyssnar på UDP {UDP_PORT}...")
 
     global latest
-    logfile = open("logs/test.influx", "a")
+    name = datetime.now().strftime("logs/trip_%Y-%m-%d_%H%M%S.influx")
+    logfile = open(name, "a")
+    buffer = []
+    last_flush = time.time()
 
     while True:
         data, addr = sock.recvfrom(1024)
@@ -31,10 +34,18 @@ def udp_listener():
             latest = d
             latest["pi_time"] = datetime.now().isoformat(timespec='milliseconds')
 
-            # Logger file
-            line = build_line(d)
-            logfile.write(line + "\n")
-            logfile.flush()
+            # Logfile
+            buffer.append(build_line(d))
+
+            if time.time() - last_flush > 1.0:
+                if buffer:
+                    logfile.write("\n".join(buffer) + "\n") #Join all rows in buffer
+                    logfile.flush()
+                    buffer.clear()
+                last_flush = time.time()
+            #line = build_line(d)
+            #logfile.write(line + "\n")
+            #logfile.flush()
 
             ######### FIXME ###############
             # Until real sensors are connected
@@ -48,8 +59,16 @@ def udp_listener():
 
 def build_line(d):
     ts = time.time_ns()
-    return f"boat rpm={d['rpm']},lift={d['lift']},trim={d['trim']},overheat={d['overheat']},oilLow={d['oilLow']},kn={d['kn']},waterpressure={d['waterpressure']},fuel={d['fuel']} {ts}"
-
+    return (f"boat "
+            f"rpm={float(d.get('rpm',0))},"
+            f"lift={float(d.get('lift',0))},"
+            f"trim={float(d.get('trim',0))},"
+            f"kn={float(d.get('kn',0))},"
+            f"waterpressure={float(d.get('waterpressure',0))},"
+            f"fuel={float(d.get('fuel',0))},"
+            f"overheat={int(d.get('overheat',0))}i,"
+            f"oilLow={int(d.get('oilLow',0))}i "
+            f"{ts}")
 # Configure Flask Webserver
 app = Flask(__name__)
 
